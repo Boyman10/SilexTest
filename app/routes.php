4437a796a07ea1054f3f9665f5dd6e54468cac82
Boyman10 <contact@yasi.fr>
2018-04-04 22:00:54 +0200

    Altering routes file to include our new form at the right place

--- app/routes.php
+++ app/routes.php
@@ -1,25 +1,46 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
-
+use SilexTest\Domain\Comment;
+use SilexTest\Form\Type\CommentType;
 
 // Home page
 $app->get('/', function () use ($app) {
     $articles = $app['dao.article']->findAll();
     return $app['twig']->render('index.html.twig', array('articles' => $articles));
-})->bind('home');// name of the route
+})->bind('home');
 
 // Article details with comments
-$app->get('/article/{id}', function ($id) use ($app) {
+$app->match('/article/{id}', function ($id, Request $request) use ($app) {
     $article = $app['dao.article']->find($id);
+    $commentFormView = null;
+    if ($app['security.authorization_checker']->isGranted('IS_AUTHENTICATED_FULLY')) {
+        // A user is fully authenticated : he can add comments
+        $comment = new Comment();
+        $comment->setArticle($article);
+        $user = $app['user'];
+        $comment->setAuthor($user);
+        $commentForm = $app['form.factory']->create(CommentType::class, $comment);
+        $commentForm->handleRequest($request);
+        if ($commentForm->isSubmitted() && $commentForm->isValid()) {
+            $app['dao.comment']->save($comment);
+            $app['session']->getFlashBag()->add('success', 'Your comment was successfully added.');
+        }
+        $commentFormView = $commentForm->createView();
+    }
     $comments = $app['dao.comment']->findAllByArticle($id);
-    return $app['twig']->render('article.html.twig', array('article' => $article, 'comments' => $comments));
-})->bind('article');// name of the route
+    
+    return $app['twig']->render('article.html.twig', array(
+        'article' => $article,
+        'comments' => $comments,
+        'commentForm' => $commentFormView));
+})->bind('article');
 
-// Login form route
+// Login form
 $app->get('/login', function(Request $request) use ($app) {
     return $app['twig']->render('login.html.twig', array(
         'error'         => $app['security.last_error']($request),
         'last_username' => $app['session']->get('_security.last_username'),
     ));
-})->bind('login'); // name of the route
+})->bind('login');
+// name of the route
